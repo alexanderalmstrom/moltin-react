31c6d2ad28fa5ac486851becc9579b3ad3127c7e
Alexander Almström <info@alexanderalmstrom.com>
2019-03-17 01:03:22 +0100

    Convert files to spaces

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,3 @@
 module.exports =  {
-	extends: 'webpack.config/babel.config'
+  extends: 'webpack.config/babel.config'
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const merge = require('webpack-merge')
 const webpack = require('webpack.config')
 
 module.exports = (env, argv) => {
-	const config = webpack(env, argv)
+  const config = webpack(env, argv)
 
-	return merge(config, {
-		
-	})
+  return merge(config, {
+    
+  })
 }
